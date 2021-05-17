a6ae4e969ab9e5567b8bbd0c5b8172585ef09993
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-05-17 17:28:58 -0300

    feat: add GlobalStyles

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Dashboard from './pages/Dashboard'
+import GlobalStyles from './styles/GlobalStyles'
+
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+   <>
+    <GlobalStyles />
+    <Dashboard />
+   </>
+  )
+}
+
+export default App
