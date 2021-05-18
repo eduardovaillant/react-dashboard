e328fd5c9b87669b6f4a5a87f7ac6d4fdcfbfe82
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-05-18 17:28:26 -0300

    feat: add default theme variables to styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import GlobalStyles from './styles/GlobalStyles'
 import Layout from './components/Layout'
+import dark from './styles/themes/dark'
 
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
 const App: React.FC = () => {
   return (
-   <>
+   <ThemeProvider theme={dark}>
     <GlobalStyles />
     <Layout />
-   </>
+   </ThemeProvider>
   )
 }
 
